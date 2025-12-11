5cc8eafaa50b8e7707016f59ee8bd98f9625f8ec
hatimbenhsain <hb1487@nyu.edu>
2025-12-11 17:51:43 -0500

    adjusted rotation degrees for hover links

--- mainPageScript.js
+++ mainPageScript.js
@@ -22,7 +22,7 @@ function styleLinks(){
         //var n=Math.round((links.length-Math.abs(links.length/2-i))/2)*2
         var n=Math.abs(Math.cos(angle*Math.PI/180))+Math.random()*0.01
         if(links[i].matches(':hover')){
-          links[i].getElementsByTagName("a")[0].style.transform="translate("+(-n*20+18)+"em"+",0)"+" rotate("+((Math.random()-.5)*45)+"deg)";
+          links[i].getElementsByTagName("a")[0].style.transform="translate("+(-n*20+18)+"em"+",0)"+" rotate("+((Math.random()-.5)*33)+"deg)";
         }else{
           links[i].getElementsByTagName("a")[0].style.transform="translate("+(-n*20+18)+"em"+",0)"+" rotate("+angle+"deg)";
         }
